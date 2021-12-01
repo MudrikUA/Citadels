a76db48fbb261bbf92e4e08c7dde524dff14a16f
Volodymyr Mudrytskyi <mudrikua@gmail.com>
2021-12-01 10:52:37 +0200

    done roles drow for 3-7 players

--- app.js
+++ app.js
@@ -3,16 +3,41 @@ class GameEngine {
         if (!GameEngine._instance) {
             GameEngine._instance = this;
             this.roles = [];
+            this.selectedHideRoles = [];
+            this.selectedUnHideRoles = [];
             this.players = [];
             this.buildings = [];
-            this.currentPlayerIndex = 3;
-            this.crownPlayerIndex = 3;
-            this.firstDoneBuildPlayerIndex = 0;
+            this.currentPlayerIndex = null;
+            this.crownPlayerIndex = null;
+            this.firstDoneBuildPlayerIndex = null;
             this.isAllRoledSelected = false;
             this.currentPlayer = null;
+            this.drowHideCard = 0;
+            this.drowUnHideCard = 0;
+            this.phases = [];
+            this.currentPhase = 0;
+
             this.iniGame();
         }
         return GameEngine._instance;
+    }
+
+    resetAllMeta() {
+        this.selectedHideRoles = [];
+        this.selectedUnHideRoles = [];
+        this.currentPlayerIndex = null;
+        this.crownPlayerIndex = null;
+        this.firstDoneBuildPlayerIndex = null;
+        this.isAllRoledSelected = false;
+        this.currentPlayer = null;
+        this.drowHideCard = 0;
+        this.drowUnHideCard = 0;
+        this.roles.forEach((role, index) => {
+            role.isSelected = false;
+        });
+        this.players.forEach((player, index) => {
+            player.role = [];
+        });
     }
 
     iniGame() {
@@ -20,7 +45,8 @@ class GameEngine {
         this.initRoles();
         this.initBuildings();
         this.initActions();
-        this.updateRolesView();
+        this.initGameActions();
+        //this.updateRolesView();
         console.log('OK');
     }
 
@@ -44,25 +70,90 @@ class GameEngine {
     }
 
     initBuildings() {
-        console.log('----------init Buildings------------');
+        console.log('----------init Buildings------------'); //  constructor(buildName, price, color, isHaveBuildingDestroyImune, imageSrc, type)
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Лавка", 2, "green", false, "", 1, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Порт", 3, "green", false, "", 2, false));
+        }
+        for (let index = 0; index < 4; index++) {
+            this.buildings.push(new Building("Рынок", 2, "green", false, "", 3, false));
+        }
+        for (let index = 0; index < 2; index++) {
+            this.buildings.push(new Building("Гавань", 4, "green", false, "", 4, false));
+        }
+        for (let index = 0; index < 2; index++) {
+            this.buildings.push(new Building("Ратуша", 5, "green", false, "", 5, false));
+        }
+        for (let index = 0; index < 5; index++) {
+            this.buildings.push(new Building("Таверна", 1, "green", false, "", 6, false));
+        }
+        for (let index = 0; index < 5; index++) {
+            this.buildings.push(new Building("Поместье", 3, "yellow", false, "", 7, false));
+        }
+        for (let index = 0; index < 4; index++) {
+            this.buildings.push(new Building("Замок", 4, "yellow", false, "", 8, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Палаццо", 5, "yellow", false, "", 9, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Церковь", 2, "blue", false, "", 10, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Монастырь", 3, "blue", false, "", 11, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Храм", 1, "blue", false, "", 12, false));
+        }
+        for (let index = 0; index < 2; index++) {
+            this.buildings.push(new Building("Собор", 5, "blue", false, "", 13, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Крепость", 5, "red", false, "", 14, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Марсово поле", 3, "red", false, "", 15, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Дозорная башня", 1, "red", false, "", 16, false));
+        }
+        for (let index = 0; index < 3; index++) {
+            this.buildings.push(new Building("Тюрма", 2, "red", false, "", 17, false));
+        }
+        //bonus
+        this.buildings.push(new Building("Форт", 3, "purple", true, "", 18, true));
+        this.buildings.push(new Building("Имперская казна", 5, "purple", false, "", 19, true));
+        this.buildings.push(new Building("Собрание карт", 5, "purple", false, "", 20, true));
+        this.buildings.push(new Building("Библиотека", 6, "purple", false, "", 21, true));
+        this.buildings.push(new Building("Обсерватория", 4, "purple", false, "", 22, true));
+        this.buildings.push(new Building("Кладбище", 5, "purple", false, "", 23, true));
+        this.buildings.push(new Building("Великая стена", 6, "purple", false, "", 24, true));
+        this.buildings.push(new Building("Квартал призраков", 2, "purple", false, "", 25, true));
+        this.buildings.push(new Building("Университет", 6, "purple", false, "", 26, true));
+        this.buildings.push(new Building("Врата дракона", 6, "purple", false, "", 27, true));
+        this.buildings.push(new Building("Школа магии", 6, "purple", false, "", 28, true));
+        this.buildings.push(new Building("Кузня", 5, "purple", false, "", 29, true));
+        this.buildings.push(new Building("Лаборатория", 5, "purple", false, "", 30, true));
 
         console.log('OK');
     }
-    
+
     initGameActions() {
-        console.log('----------init roles------------');
-        this.roles.push(new GameActions('Выбор класса', 1));
-        this.roles.push(new GameActions('Збор ресурсов', 2));
-        this.roles.push(new GameActions('Строительство', 3));
-        this.roles.push(new GameActions('Особое свойство персонажа', 4));
+        console.log('----------init initGameActions------------');
+        this.phases.push(new GameActions('Выбор класса', 1));
+        this.phases.push(new GameActions('Збор ресурсов', 2));
+        this.phases.push(new GameActions('Строительство', 3));
+        this.phases.push(new GameActions('Особое свойство персонажа', 4));
         console.log('OK');
     }
 
     joinToGame() {
         //var gameEngine = new GameEngine();
         console.log('----------joinToGame------------');
-        if (document.querySelector('#pname') && this.players.length < 8) {
-            let newPlayer = new Player(document.querySelector('#pname').value, 2, [], [], null, false);
+        if (document.querySelector('#pname') && this.players.length < 7) {
+            let newPlayer = new Player(document.querySelector('#pname').value, 2, [], getRandomFourStartingBuildings(), [], false);
             this.players.push(newPlayer);
             this.updatePlayerView();
         }
@@ -72,37 +163,97 @@ class GameEngine {
 
     }
 
-    starGame() {
-        console.log('----------starGame------------');
-        this.selectRolesStep();
+    selectStartPlayer(player) {
+        this.currentPlayerIndex = player.dataset.id;
+        this.crownPlayerIndex = player.dataset.id;
+        this.updatePlayerView();
     }
 
-    selectRolesStep() {
-        let player = this.getNextPlayerInOneTurn();
-        if (player) {
-            this.selectedPlayer = player;
-            console.log(player.playerName);
-            this.updateCurrentPlayerInfo();
+    performNewPhase() {
+        console.log(this.buildings);
+        if (this.players.length < 3) {
+            console.log("мало гравців");
         } else {
-            this.isAllRoledSelected = true;
-        }
+            if (this.currentPhase === 0) {
+                this.updateRolesView();
+                this.updateCurrentPlayerInfo();
+                this.drowRoleAutomaticaly(this.getCountOfUnhideRole(), false);
+                this.drowRoleAutomaticaly(1, true);
+            } else if (this.currentPhase === 1) {
 
+            } else if (this.currentPhase === 2) {
+
+            } else if (this.currentPhase === 3) {
+
+            }
+        }
     }
 
     selectRole(role) {
-        if (this.players.length === 2) {
+        if (this.currentPhase === 0) {
+            let player = this.getNextPlayerInOneTurnForRoleSelect();
             let selectedRole = this.roles[role.dataset.id];
-            selectedRole.isSelected = true;
+            if (player && !selectedRole.isSelected) {
+                this.currentPlayer = player;
+                if (this.players.length === 2) {
+
+                } else if (this.players.length >= 3) {
+                    selectedRole.isSelected = true;
+                    this.currentPlayer.role.push(selectedRole);
+                } else if (this.players.length < 3) {
+                    console.log("мало гравців");
+                }
+                if (this.players.length === 7) {
+                    this.specialRuleForSeventhPlayer();
+                }
+            }
+            if (!player || this.getUnselectedRolesCount() === 1) {
+                this.currentPhase = 1;
+            }
             this.updateRolesView();
-            this.currentPlayer.role = selectedRole;
-            console.log('----------starGame------------ ' + selectedRole.roleName);
-        } else if (this.players.length === 3) {
-
-        } else if (this.players.length > 3) {
-
-        } else if (this.players.length < 2) {
-            console.log("мало гравців");
+            this.updatePlayerView();
+            this.updateCurrentPlayerInfo();
         }
+    }
+
+    specialRuleForSeventhPlayer() {
+        if (this.getUnselectedRolesCount() === 1) {
+            if (this.selectedHideRoles.length > 0) {
+                var hideRole = this.selectedHideRoles[0];
+                var role = this.getRoleBySequenceOfMoves(hideRole.sequenceOfMoves);
+                role.isSelected = false;
+                this.selectedHideRoles = [];
+            }
+        }
+    }
+
+    drowRoleAutomaticaly(numberOfRoles, isHide) {
+        for (let i = 0; i < numberOfRoles; ++i) {
+            let randomRole = getRandomInt(0, 7);
+            console.log("random-> " + randomRole);
+            if (randomRole === 3 || this.roles[randomRole].isSelected) {
+                --i;
+            } else if (!this.roles[randomRole].isSelected) {
+                var selectedRole = this.roles[randomRole]
+                selectedRole.isSelected = true;
+                if (isHide) {
+                    this.selectedHideRoles.push(selectedRole);
+                } else {
+                    this.selectedUnHideRoles.push(selectedRole);
+                }
+            }
+        }
+        this.updateRolesView();
+    }
+
+
+    getCountOfUnhideRole() {
+        if (this.players.length === 4) {
+            return 2;
+        } else if (this.players.length === 5) {
+            return 1;
+        }
+        return 0;
 
     }
 
@@ -112,16 +263,52 @@ class GameEngine {
         });
     }
 
-    getNextPlayerInOneTurn() {
-        let nextPlayer = ++this.currentPlayerIndex
-        if (nextPlayer >= this.players.length && this.crownPlayerIndex !== 0) {
-            nextPlayer = 0;
-        } else if ((nextPlayer >= this.players.length && this.crownPlayerIndex === 0) ||
-            nextPlayer === this.crownPlayerIndex) {
+    getNextPlayerInOneTurnForRoleSelect() {
+        let maxPlayerRoles = this.players.length > 3 ? 1 : 2;
+        let player = this.players[this.currentPlayerIndex];
+        if (!player && this.players[0].role.length !== maxPlayerRoles) {
+            this.currentPlayerIndex = 1;
+            return this.players[0];
+        } else if (!player || player.role.length === maxPlayerRoles) {
             return null;
         }
-        this.currentPlayerIndex = nextPlayer;
-        return this.players[nextPlayer];
+        this.currentPlayerIndex = ++this.currentPlayerIndex;
+        return player;
+    }
+
+    getNextPlayerInOneTurnByRole() {
+        let maxPlayerRoles = this.players.length > 3 ? 1 : 2;
+        let player = this.players[this.currentPlayerIndex];
+        if (!player && this.players[0].role.length !== maxPlayerRoles) {
+            this.currentPlayerIndex = 1;
+            return this.players[0];
+        } else if (!player || player.role.length === maxPlayerRoles) {
+            return null;
+        }
+        this.currentPlayerIndex = ++this.currentPlayerIndex;
+        return player;
+    }
+
+    getUnselectedRolesCount() {
+        let count = 0;
+        this.roles.forEach((role, index) => {
+            if (!role.isSelected) {
+                count++;
+            }
+        });
+        return count;
+    }
+
+    getRoleBySequenceOfMoves(sequence) {
+        for (let i = 0; i < this.roles.length; i++) {
+            if (this.roles[i].sequenceOfMoves === sequence) {
+                return this.roles[i];
+            }
+        }
+    }
+
+    getRandomFourStartingBuildings() {
+
     }
 
     //--------game functionality -------------
@@ -135,7 +322,7 @@ class GameEngine {
     getBonusCoin() { console.log('8') }
     spendCoin(count) { console.log('9') }
     getCard() { console.log('10') }
-    discardACard() { console.log('11') }
+    discardCard() { console.log('11') }
     buildOneBuilding() { console.log('12') }
     destroyBuilding() { console.log('13') }
     checkIfPossibleToDestroyBuilding() { console.log('14') }
@@ -148,9 +335,21 @@ class GameEngine {
     updatePlayerView() {
         console.log('----------updatePlayerView------------');
         document.querySelector('.js-players-list').innerHTML = "";
-        this.players.forEach((element) => {
+        this.players.forEach((element, index) => {
             var player = document.createElement("div");
-            player.innerHTML = '<div onclick="game.leftGame()">' + element.playerName + '</div>';
+            let playerRoles = "";
+            element.role.forEach((role) => {
+                playerRoles += role.roleName;
+            });
+            if (!this.crownPlayerIndex) {
+                player.innerHTML = '<div onclick="game.selectStartPlayer(this)" data-id="' + index +
+                    '">' + element.playerName + " -> " + playerRoles + " -> " + element.coins + " -> " +
+                    element.buildingsInHand + " -> " + element.buildingsInTown + '</div>';
+            } else {
+                player.innerHTML = '<div >' + element.playerName + " -> " + playerRoles + element.coins + " -> " +
+                    element.buildingsInHand + " -> " + element.buildingsInTown + '</div>';
+            }
+            this.crownPlayerIndex === '' + index ? player.style.color = "gold" : '';
             document.querySelector('.js-players-list').appendChild(player);
         });
     }
@@ -158,26 +357,46 @@ class GameEngine {
     updateRolesView() {
         console.log('----------updateRolesView------------');
         document.querySelector('.js-roles-list').innerHTML = "";
+        document.querySelector('.js-hide-roles-list').innerHTML = "";
+        document.querySelector('.js-unhide-roles-list').innerHTML = "";
         this.roles.forEach((element, index) => {
             var role = document.createElement("div");
-            if (!element.isSelected) {
-                role.innerHTML = '<div onclick="game.selectRole(this)" data-id="' + index + '">' + element.roleName + '</div>';
+            if (!element.isSelected && this.currentPhase === 0) {
+                role.innerHTML = '<div style="color:green" onclick="game.selectRole(this)" data-id="' + index + '">' + element.roleName + '</div>';
             } else {
-                role.innerHTML = '<div data-id="' + index + '">' + element.roleName + '</div>';
+                //role.innerHTML = '<div style="color:red" data-id="' + index + '">' + element.roleName + '</div>';
             }
             document.querySelector('.js-roles-list').appendChild(role);
+        });
+        this.selectedHideRoles.forEach((element, index) => {
+            var role = document.createElement("div");
+            role.innerHTML = '<div>' + element.roleName + '</div>';
+            document.querySelector('.js-hide-roles-list').appendChild(role);
+        });
+        this.selectedUnHideRoles.forEach((element, index) => {
+            var role = document.createElement("div");
+            role.innerHTML = '<div>' + element.roleName + '</div>';
+            document.querySelector('.js-unhide-roles-list').appendChild(role);
         });
     }
 
     updateCurrentPlayerInfo() {
-        document.querySelector('.js-current-player').innerHTML = this.currentPlayer ? "Ход игрока" + this.currentPlayer.playerName : "";
-        document.querySelector('.js-current-player-action').innerHTML = "test action";
+        let player = this.players[this.currentPlayerIndex];
+        document.querySelector('.js-current-player').innerHTML = player ? "Ход игрока -> " + player.playerName : "";
+        document.querySelector('.js-current-player-action').innerHTML = "test action -> " + this.currentPlayerIndex;
     }
 
     static getInstance() {
         return this._instance;
     }
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
 
 class Role {
     constructor(roleName, sequenceOfMoves, isHaveBuildingDestroyImune, imageSrc, isSelected) {
@@ -201,14 +420,14 @@ class Player {
 }
 
 class Building {
-    constructor(buildName, price, color, sequenceOfMoves, isHaveBuildingDestroyImune, imageSrc) {
+    constructor(buildName, price, color, isHaveBuildingDestroyImune, imageSrc, type, isSpecialCard) {
         this.buildName = buildName;
         this.price = price;
         this.color = color;
-        this.sequenceOfMoves = sequenceOfMoves;
-        this.roleName = roleName;
         this.isHaveBuildingDestroyImune = isHaveBuildingDestroyImune;
         this.imageSrc = imageSrc;
+        this.type = type;
+        this.isSpecialCard = isSpecialCard;
     }
 }
 
